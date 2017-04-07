a05d1a2cdb0092cd4c0cca09c7c62103239c40f0
Carsten Knappe <carsten.knappe@psmtec.de>
2017-04-07 16:46:22 +0200

    Update

--- util/separators.lua
+++ util/separators.lua
@@ -17,7 +17,7 @@ local separators = { height = 0, width = 9 }
 -- [[ Arrow
 
 -- Right
-function separators.arrow_right(col1, col2)
+function separators.arrow_right(col1, col2, fill)
     local widget = wibox.widget.base.make_widget()
 
     widget.fit = function(m, w, h)
@@ -25,14 +25,22 @@ function separators.arrow_right(col1, col2)
     end
 
     widget.draw = function(mycross, wibox, cr, width, height)
-        if col2 ~= "alpha" then
-            cr:set_source_rgb(gears.color.parse_color(col2))
+	local col = nil
+	if col2 ~= "alpha" then
+	    cr:set_source(gears.color(col2))
             cr:new_path()
             cr:move_to(0, 0)
             cr:line_to(width, height/2)
             cr:line_to(width, 0)
+	    if not fill then
+	       cr:move_to( 0, 0)
+	    end
             cr:close_path()
-            cr:fill()
+	    if fill then
+	       cr:fill()
+	    else
+	       cr:stroke()
+	    end
 
             cr:new_path()
             cr:move_to(0, height)
@@ -40,24 +48,45 @@ function separators.arrow_right(col1, col2)
             cr:line_to(width, height)
             cr:close_path()
             cr:fill()
+	    col = col2
         end
 
         if col1 ~= "alpha" then
-            cr:set_source_rgb(gears.color.parse_color(col1))
+	    cr:set_source(gears.color(col1))
             cr:new_path()
             cr:move_to(0, 0)
             cr:line_to(width, height/2)
             cr:line_to(0, height)
+	    if not fill then
+	       cr:move_to( 0, 0)
+	    end
             cr:close_path()
-            cr:fill()
+	    if fill then
+	       cr:fill()
+	    else
+	       cr:stroke()
+	    end
+	    col = col1
         end
+	if col and fill then
+	    cr:set_source(gears.color(col))
+            cr:new_path()
+
+            cr:move_to(0, 0)
+            cr:line_to(width, height/2)
+            cr:line_to(0, height)
+	    cr:move_to( 0, 0)
+
+            cr:close_path()
+	    cr:stroke()
+	end
    end
 
    return widget
 end
 
 -- Left
-function separators.arrow_left(col1, col2)
+function separators.arrow_left(col1, col2, fill)
     local widget = wibox.widget.base.make_widget()
 
     widget.fit = function(m, w, h)
@@ -65,33 +94,61 @@ function separators.arrow_left(col1, col2)
     end
 
     widget.draw = function(mycross, wibox, cr, width, height)
+	local col = nil
         if col1 ~= "alpha" then
-            cr:set_source_rgb(gears.color.parse_color(col1))
+	    cr:set_source(gears.color(col1))
             cr:new_path()
-            cr:move_to(width, 0)
+	    if fill then
+		cr:move_to(0, 0)
+	    end
+            cr:line_to(width, 0)
             cr:line_to(0, height/2)
-            cr:line_to(0, 0)
+            cr:line_to(width, height)
+	    if fill then
+		cr:line_to(0, height)
+	    else
+		cr:move_to(width, 0)
+	    end
             cr:close_path()
-            cr:fill()
-
-            cr:new_path()
-            cr:move_to(width, height)
-            cr:line_to(0, height/2)
-            cr:line_to(0, height)
-            cr:close_path()
-            cr:fill()
+	    if fill then
+	       cr:fill()
+	    else
+	       cr:stroke()
+	    end
+	    col = col
         end
 
         if col2 ~= "alpha" then
+	    cr:set_source(gears.color(col2))
             cr:new_path()
             cr:move_to(width, 0)
             cr:line_to(0, height/2)
             cr:line_to(width, height)
+	    if not fill then
+		cr:move_to(width, 0)
+	    end
             cr:close_path()
 
-            cr:set_source_rgb(gears.color.parse_color(col2))
-            cr:fill()
+	    if fill then
+	       cr:fill()
+	    else
+	       cr:stroke()
+	    end
+	    col = col2
         end
+
+	if col and fill then
+	    cr:set_source(gears.color(col))
+            cr:new_path()
+
+            cr:move_to(width, 0)
+            cr:line_to(0, height/2)
+            cr:line_to(width, height)
+	    cr:move_to(width, 0)
+
+            cr:close_path()
+	    cr:stroke()
+	end
    end
 
    return widget
